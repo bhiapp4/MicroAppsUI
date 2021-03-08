a910632afafd100849ee8bd1e4c246f59d8164e0
bhiapp4 <bhiapp4@gmail.com>
2021-03-07 21:47:47 -0600

    Revert "Make things works"

--- projects/shell/src/app/app.component.ts
+++ projects/shell/src/app/app.component.ts
@@ -1,34 +1,10 @@
-import { Component, getModuleFactory, Injector, OnInit } from '@angular/core';
-import { from } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'shell';
-
-  constructor(readonly injector: Injector) {
-  }
-
-  ngOnInit() {
-    for (const script of ['shared.js', 'app1.js', 'app2.js']) {
-      this.addScript(script);
-    }
-
-    setTimeout(() => {
-      const app1Module = getModuleFactory('app1').create(this.injector);
-      app1Module.instance.ngDoBootstrap();
-
-      const app2Module = getModuleFactory('app2').create(this.injector);
-      app2Module.instance.ngDoBootstrap();
-    }, 2000);
-  }
-
-  addScript(src: string) {
-    const script = document.createElement('script');
-    script.src = src;
-    document.head.appendChild(script);
-  }
 }
--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -1,22 +1,12 @@
-import * as core from '@angular/core';
-import * as common from '@angular/common';
-import * as platformBrowser from '@angular/platform-browser';
-import * as animations from '@angular/animations';
-import * as router from '@angular/router';
-import * as elements from '@angular/elements';
-import * as http from '@angular/common/http';
-import * as rxjs from 'rxjs';
-import * as operators from 'rxjs/operators';
-
-(window as any).rxjs = {...rxjs, operators};
-(window as any).ng = {core, common: {...common, http}, platformBrowser, animations, router, elements};
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
-  core.enableProdMode();
+  enableProdMode();
 }
 
-platformBrowser.platformBrowser().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
